8fe5e5c8c4e9f036f49b9f3fa8a8a1fe7e2316d5
Himanshu-kr-10 <139107004+Himanshu-kr-10@users.noreply.github.com>
2024-02-15 17:01:45 +0530

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     }
   };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/bgimage.jpeg')]">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/bgimage.jpeg')] bg-center">
       <div className="flex justify-between w-[900px] p-5">
         <div className="font-bold text-xl"></div>
         <button className="bg-rose-500 px-4 py-2 rounded-lg text-white">Connect wallet</button>
